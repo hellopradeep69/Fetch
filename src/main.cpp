715ad5c5a582a9263b3d3e976582aac8fb3b57d6
amark2005 <amarkeshav14@gmail.com>
2026-03-16 22:08:09 +0530

    cout bottleneck fixed

--- src/main.cpp
+++ src/main.cpp
@@ -146,6 +146,8 @@ void Title() {
 void Seperator() { std::cout << "-----------------\n"; }
 
 int main() {
+  std::ios_base::sync_with_stdio(false);
+  std::cin.tie(NULL);
   Title();
   Seperator();
   Osname();
